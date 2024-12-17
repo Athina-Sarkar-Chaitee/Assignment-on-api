a421a2169fb40ce482fd629ef7657609ad066be2
Athina Sarkar <athinasarkarchaitee@Athinas-MacBook-Air.local>
2024-12-18 02:25:42 +0600

    weather done

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-function find(){
+function find() {
+    let name = document.getElementById("nameBox").value;
 
-    let name = document.getElementById("nameBox").value ;
-
-    let url = `https://restcountries.com/v3.1/name/${name}` ;
+    let url = `https://restcountries.com/v3.1/name/${name}`;
 
     fetch(url)
-    .then(res => res.json())
-    .then(data => find2(data));
-
-
+        .then(res => res.json())
+        .then(data => find2(data));
 }
 
-function find2(data){
-
+function find2(data) {
     let disArea = document.getElementById("dis1");
-    disArea.textContent = "" ;
+    disArea.textContent = "";
 
-    for(let i = 1; i <= data.length ; i++){
-
+    for (let i = 0; i < data.length; i++) {
         let newDiv = document.createElement("div");
-        newDiv.innerHTML = `Name : ${data[i-1].name.common} <br> Capital : ${data[i-1].capital} <br> Population : ${data[i-1].population} <br> Flag : <br> <img src="${data[i-1].flags.png}">`
-
+        newDiv.innerHTML = `Name: ${data[i].name.common} <br> 
+                            Capital: ${data[i].capital} <br> 
+                            Population: ${data[i].population} <br> 
+                            Flag: <br> 
+                            <img src="${data[i].flags.png}" alt="Flag"> <br> 
+                            <button onclick="weather('${data[i].capital}')">See weather</button>`;
 
         newDiv.classList.add("style");
         disArea.appendChild(newDiv);
     }
+}
+
+function weather(capital) {
+    let url = `http://api.weatherapi.com/v1/current.json?key=5ebcc74ed0da4ce68ea85758240312&q=${capital}&aqi=no`;
+
+    fetch(url)
+        .then(res2 => res2.json())
+        .then(data2 => weather2(data2));
+}
+
+function weather2(data2) {
+    let disArea2 = document.getElementById("dis2");
+    disArea2.textContent = ""; // Clear the previous weather info
+
+    let newDiv2 = document.createElement("div");
+    newDiv2.innerHTML = `Capital: ${data2.location.name} <br> 
+                         Temp: ${data2.current.temp_c}°C <br> 
+                         Condition: ${data2.current.condition.text} <br> 
+                         <img src="${data2.current.condition.icon}" alt="Weather icon">`;
+
+    newDiv2.classList.add("style2");
+    disArea2.appendChild(newDiv2);
 }
